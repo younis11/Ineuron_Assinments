caec6757d558179bdae6ef3a8956162399153c7f
younis11 <younisrafiq@gmail.com>
2023-11-03 12:33:09 +0300

    Update Assignment exception handling.py

--- Assignment exception handling.py
+++ Assignment exception handling.py
@@ -8,3 +8,17 @@ def divide_by_zero():
 
 result = divide_by_zero()
 print(result)
+
+
+def generate_all_sentences():
+    subjects = ["Americans", "Indians"]
+    verbs = ["play", "watch"]
+    objects = ["Baseball", "Cricket"]
+
+    for subject in subjects:
+        for verb in verbs:
+            for obj in objects:
+                sentence = f"{subject} {verb} {obj}."
+                print(sentence)
+                
+generate_all_sentences()
